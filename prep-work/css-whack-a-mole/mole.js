e350019e6518c59ba8c8de85be9fddcf78699bcb
codedrian <adriangailee@gmail.com>
2023-02-01 20:47:24 +0800

    Add the snippet code to animate the disappearing mole head

--- prep-work/css-whack-a-mole/mole.js
+++ prep-work/css-whack-a-mole/mole.js
@@ -59,3 +59,12 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+	setInterval(() => {
+		const moleHeads = document.querySelectorAll('.wgs__mole-head');
+		for (let moleHead of moleHeads) {
+			moleHead.classList.toggle('wgs__mole-head--hidden');
+		}
+	}, 1000);
+});
