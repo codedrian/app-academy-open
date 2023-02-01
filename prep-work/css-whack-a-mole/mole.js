6b6be1225c04af50275deb2108dec2461d908237
codedrian <adriangailee@gmail.com>
2023-02-01 22:13:02 +0800

    UNcomment code fore score board

--- prep-work/css-whack-a-mole/mole.js
+++ prep-work/css-whack-a-mole/mole.js
@@ -5,61 +5,64 @@ let hideTimeout;
 let clickable = false;
 
 function popUpRandomMole() {
-  if (molesLeft <= 0) {
-    document.querySelector('.sb__game-over').classList.remove('sb__game-over--hidden');
-    return;
-  }
+	if (molesLeft <= 0) {
+		document
+			.querySelector('.sb__game-over')
+			.classList.remove('sb__game-over--hidden');
+		return;
+	}
 
-  const moleHeads = document.querySelectorAll('.wgs__mole-head');
+	const moleHeads = document.querySelectorAll('.wgs__mole-head');
 
-  if (moleHeads.length === 0) {
-    return;
-  }
-  const moleIndex = Math.floor(Math.random() * moleHeads.length);
-  const moleHead = moleHeads[moleIndex];
+	if (moleHeads.length === 0) {
+		return;
+	}
+	const moleIndex = Math.floor(Math.random() * moleHeads.length);
+	const moleHead = moleHeads[moleIndex];
 
-  clickable = true;
+	clickable = true;
 
-  // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-  // moleHead.classList.remove('wgs__mole-head--hidden', 'wgs__mole-head--whacked');
+	moleHead.classList.remove(
+		'wgs__mole-head--hidden',
+		'wgs__mole-head--whacked'
+	);
 
-  molesLeft -= 1;
-  document.querySelector('.sb__moles').innerHTML = molesLeft;
+	molesLeft -= 1;
+	document.querySelector('.sb__moles').innerHTML = molesLeft;
 
-  hideTimeout = setTimeout(() => hideMole(moleHead), popupLength);
+	hideTimeout = setTimeout(() => hideMole(moleHead), popupLength);
 }
 
 function hideMole(mole) {
-  clickable = false;
-  mole.classList.add('wgs__mole-head--hidden');
+	clickable = false;
+	mole.classList.add('wgs__mole-head--hidden');
 
-  setTimeout(popUpRandomMole, 500);
+	setTimeout(popUpRandomMole, 500);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-  setTimeout(popUpRandomMole, 0);
+	setTimeout(popUpRandomMole, 0);
 
-  const moleHeads = document.querySelectorAll('.wgs__mole-head');
-  for (let moleHead of moleHeads) {
-    moleHead.addEventListener('click', event => {
-      if (!clickable) return;
+	const moleHeads = document.querySelectorAll('.wgs__mole-head');
+	for (let moleHead of moleHeads) {
+		moleHead.addEventListener('click', (event) => {
+			if (!clickable) return;
 
-      score += 1;
-      document.querySelector('.sb__score').innerHTML = score;
-      popupLength -= popupLength / 10;
+			score += 1;
+			document.querySelector('.sb__score').innerHTML = score;
+			popupLength -= popupLength / 10;
 
-      clearTimeout(hideTimeout);
-      hideMole(event.target);
+			clearTimeout(hideTimeout);
+			hideMole(event.target);
 
-      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED
-      // event.target.classList.add('wgs__mole-head--hidden');
+			event.target.classList.add('wgs__mole-head--hidden');
 
-      // UNCOMMENT THIS LINE OF CODE WHEN DIRECTED FOR THE BONUS
-      // event.target.classList.add('wgs__mole-head--whacked');
-    });
-  }
+			//mole whacked
+			event.target.classList.add('wgs__mole-head--whacked');
+		});
+	}
 });
-
+// Animating the disappearing mole head
 window.addEventListener('DOMContentLoaded', () => {
 	setInterval(() => {
 		const moleHeads = document.querySelectorAll('.wgs__mole-head');
